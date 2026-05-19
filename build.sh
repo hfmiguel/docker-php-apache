ff237feae96e79f6f2abd864f0cf71f9e1224eaf
Henrique Felix <henrique.felix@cmore.pt>
2026-05-19 19:09:38 +0100

    update docker image .

--- build.sh
+++ build.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+cd /home/felix/Projetos/Pessoal/GithubPessoal/docker-php-apache
+
 # Define the image name
 IMAGE_NAME="devfelixh/php-8.4-apache:latest"
 
-# Navigate to the Dockerfile directory
-cd /home/felix/projetos/pessoal/docker/php-base
+# Build the Docker image
+docker build -t $IMAGE_NAME -f Dockerfile.apache .
+
+# Verify the image was created successfully
+docker images | grep $IMAGE_NAME
+
+docker push $IMAGE_NAME
+
+
+# Define the image name
+IMAGE_NAME="devfelixh/php-8.4-fpm:latest"
 
 # Build the Docker image
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME -f Dockerfile.fpm .
 
 # Verify the image was created successfully
 docker images | grep $IMAGE_NAME
